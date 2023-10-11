c367edda1c6569b46af0f11bfa25c3d248ac6fec
nicholsons <sarah.sanders@ucl.ac.uk>
2023-10-11 08:54:39 +0100

    updating 2023

---
+++ src/starter_code_t3/problem5.py
@@ -0,0 +1,77 @@
+# Problem 5: Identify and address missing values
+# Problem 4: Identify and address any missing values
+from pathlib import Path
+
+import pandas as pd
+
+
+def create_dataframe(csv_file):
+    """ Creates, prints and returns a pandas dataframe containing data from a csv file
+
+    Args:
+        csv_file: The raw data in csv format
+
+    Returns:
+        df: A pandas dataframe with the data
+
+    """
+    df = pd.read_csv(csv_file)
+    pd.set_option('display.max_rows', df.shape[0] + 1)
+    pd.set_option('display.max_columns', df.shape[1] + 1)
+    # print("\nDataFrame contents:\n", df)
+    return df
+
+
+def print_df_information(df):
+    """ Prints information that the describes the contents of the DataFrame.
+
+    Args:
+        df: A pandas dataframe containing the data
+    """
+    print("\nNumber of rows and columns:\n")
+    print(df.shape)
+    print("\nFirst 7 rows:\n")
+    print(df.head(7))
+    print("\nLast 6 rows:\n")
+    print(df.tail(6))
+    print("\nColumn labels:\n")
+    print(df.columns)
+    print("\nColumn labels, datatypes and value counts:\n")
+    print(df.info())
+    print("\nColumn data types:\n")
+    print(df.dtypes)
+    print("\nGeneral Statistics:\n")
+    print(df.describe())
+
+
+def prepare_data(df):
+    """ Takes the raw data and prepares it for later use in the paralympics dashboard
+
+       Args:
+           df: The raw data in a pandas DataFrame
+
+       Returns:
+        df_prepared: A pandas DataFrame with the prepared data
+
+       """
+    # Drop the list of named columns `['Events', 'Sports', 'Countries'] and
+    # assign the result to a new variable named df_prepared
+    df_prepared = df.drop(['Events', 'Sports', 'Countries'], axis=1)
+
+    # 1. Find and count the number of missing values e.g., Null `isnull().sum().sum()` or NaN `isna().sum().sum()`
+    # and print the result
+
+    # 2. Create a dataframe with the rows that contain missing values and print it
+
+    # 3. Drop rows where there is a na in the Participants M or F columns
+
+    # 4. Replace the NaN in Type column with 'Winter'
+
+    return df_prepared
+
+
+if __name__ == '__main__':
+    raw_data_file = Path(__file__).parent.parent.joinpath('data', 'paralympics_raw.csv')
+    raw_df = create_dataframe(raw_data_file)
+    # print_df_information(raw_df)
+    prepared_df = prepare_data(raw_df)
